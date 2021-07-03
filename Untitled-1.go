6959db68580f79c22b31ba19b9aab0d3eabda4a5
payalpatil22 <payal.h.patil@outlook.com>
2021-07-03 20:40:05 +0200

    first commit

---
+++ Untitled-1.go
@@ -0,0 +1,22 @@
+nc, err := nats.Connect("demo.nats.io")
+if err != nil {
+    log.Fatal(err)
+}
+defer nc.Close()
+
+ec, err := nats.NewEncodedConn(nc, nats.JSON_ENCODER)
+if err != nil {
+    log.Fatal(err)
+}
+defer ec.Close()
+
+// Define the object
+type stock struct {
+    Symbol string
+    Price  int
+}
+
+// Publish the message
+if err := ec.Publish("updates", &stock{Symbol: "GOOG", Price: 1200}); err != nil {
+    log.Fatal(err)
+}
